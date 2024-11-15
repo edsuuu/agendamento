b372709c97d541a1ec4940130aa64b06293334d2
edsuuu <edsonteles343@gmail.com>
2024-11-14 21:05:38 -0300

    add livewire

--- resources/views/business/dashboard.blade.php
+++ resources/views/business/dashboard.blade.php
@@ -1,3 +1,9 @@
 <div>
     <h1>Business dashboard</h1>
+    @if(auth()->check())
+        <div>
+            <h1>Logado</h1>
+            <a href="{{ route('logout') }}">Sair</a>
+        </div>
+    @endif
 </div>
