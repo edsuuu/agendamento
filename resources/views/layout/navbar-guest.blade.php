da62a9a78b62a84cb83d3824fe458238476eefc8
edsuuu <edsonteles343@gmail.com>
2025-01-29 00:05:10 -0300

    fix navbar

--- resources/views/layout/navbar-guest.blade.php
+++ resources/views/layout/navbar-guest.blade.php
@@ -2,12 +2,7 @@
     class="w-full border-b border-b-blue-link bg-blue-black text-white flex flex-row justify-between px-10 items-center p-4"
     id="navbar">
     <div>
-
-        @if(auth()->check() && (auth()->user()->role !== 'admin' && !auth()->user()->business))
-            <h2><a href="{{ route('home') }}">Logo</a></h2>
-        @else
-            <h2><a href="{{ route('dashboard') }}">Logo</a></h2>
-        @endif
+        <h2><a href="{{ route('home') }}">Logo</a></h2>
     </div>
     <div class="flex flex-row list-none gap-4">
         <ul class="flex gap-7">
@@ -18,14 +13,12 @@
     </div>
     @if(auth()->check())
         <div class="flex flex-row gap-2">
-            @if(auth()->user()->role === 'customer')
                 @if((auth()->user()->business || auth()->user()->role === 'admin') && !request()->is('complete-profile'))
                     <h1><a href="{{ route('dashboard') }}">Dashboard</a></h1>
                 @else
                   <h1><a href="{{ route('complete-profile') }}">Complete seu Cadastro</a></h1>
                 @endif
             <a href="{{ route('logout') }}">Sair</a>
-            @endif
         </div>
     @else
         <div class="flex flex-row gap-4 items-center">
