c3f5debb85db467542846b6d0c7ab4106d46f54f
edsuuu <edsonteles343@gmail.com>
2025-01-29 21:22:15 -0300

    ajuste navbar

---
+++ app/Livewire/Scheduling/NavbarGuest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Scheduling;
+
+use Livewire\Component;
+
+class NavbarGuest extends Component
+{
+    public function render()
+    {
+        return view('livewire.scheduling.navbar-guest');
+    }
+}
---
+++ resources/views/livewire/scheduling/navbar-guest.blade.php
@@ -0,0 +1,31 @@
+<nav
+    class="w-full border-b border-b-blue-link bg-blue-black text-white flex flex-row justify-between px-10 items-center p-4"
+    id="navbar">
+    <div>
+        <h2><a href="{{ route('home') }}">Logo</a></h2>
+    </div>
+    <div class="flex flex-row list-none gap-4">
+        <ul class="flex gap-7">
+            <li><a href="" class="hover:underline font-medium">Soluções</a></li>
+            <li><a href="" class="hover:underline font-medium">Recursos</a></li>
+            <li><a href="" class="hover:underline font-medium">Planos</a></li>
+        </ul>
+    </div>
+    @if(auth()->check())
+        <div class="flex flex-row gap-2">
+            @if((auth()->user()->business || auth()->user()->role === 'admin') && !request()->is('complete-profile'))
+                <h1><a href="{{ route('dashboard') }}">Dashboard</a></h1>
+            @else
+                <h1><a href="{{ route('complete-profile') }}">Complete seu Cadastro</a></h1>
+            @endif
+            <a href="{{ route('logout') }}">Sair</a>
+        </div>
+    @else
+        <div class="flex flex-row gap-4 items-center">
+            <a href="{{ route('login') }}" class="hover:underline font-medium">Login</a>
+            <button class="py-[5px] px-[17px] bg-blue-white rounded-[6px] font-medium active:scale-[0.97]" onclick="window.location='{{ route('register') }}'">
+                Comece já
+            </button>
+        </div>
+    @endif
+</nav>
