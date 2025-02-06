ab9c47bcf3ea204bf71a85c7548f92576952098d
edsuuu <edsonteles343@gmail.com>
2025-02-05 23:37:20 -0300

    adicionando tela de produtos/categorias, e feito o crud

--- app/Livewire/Scheduling/Catalog/Products.php
+++ app/Livewire/Scheduling/Catalog/Products.php
@@ -2,17 +2,66 @@
 
 namespace App\Livewire\Scheduling\Catalog;
 
+use App\Models\ProductCategory;
 use Livewire\Attributes\On;
 use Livewire\Component;
+use App\Models\Products as ProductsBusiness;
+use Livewire\WithPagination;
 
 class Products extends Component
 {
-    public $openModal = false;
+    use WithPagination;
+
+    protected $listeners = [
+        'refreshNewProducts' => '$refresh',
+    ];
+
+    public $openModal = false, $openModalCategory = false, $viewCategory = false;
+    public $selectedProductForEdit, $selectedProductForDelete, $selectedCategoryForEdit, $selectedCategoryForDelete;
+
 
     #[On('closeModalProduct')]
-    public function openAndCloseModal()
+    public function openAndCloseModal($id = null, $deleteId = null)
     {
+        if ($id != null) {
+            $this->selectedProductForEdit = $id;
+        }
+
+        if ($deleteId != null) {
+            $this->selectedProductForDelete = $deleteId;
+        }
+
         $this->openModal = !$this->openModal;
+
+        if (!$this->openModal) {
+            $this->selectedProductForEdit = null;
+            $this->selectedProductForDelete = null;
+        }
+    }
+
+    #[On(['closeModalCategory', 'openModalCategory'])]
+    public function openAndCloseModalCategory($id = null, $deleteId = null):void
+    {
+        if ($id != null) {
+            $this->selectedCategoryForEdit = $id;
+        }
+
+        if ($deleteId != null) {
+            $this->selectedCategoryForDelete = $deleteId;
+        }
+
+        $this->openModalCategory = !$this->openModalCategory;
+
+        if (!$this->openModalCategory) {
+            $this->selectedCategoryForEdit = null;
+            $this->selectedCategoryForDelete = null;
+        }
+    }
+
+
+    public function viewCategoriesAndProducts(): void
+    {
+        $this->viewCategory = !$this->viewCategory;
     }
 
     public function mount()
@@ -22,6 +71,21 @@ class Products extends Component
 
     public function render()
     {
-        return view('livewire.scheduling.catalog.products');
+        $products = ProductsBusiness::query()
+            ->with('category')
+            ->where('business_id', auth()->user()->business->id)
+            ->orderBy('name', 'ASC')
+            ->paginate(10);
+
+        $categories = ProductCategory::query()
+            ->where('business_id', auth()->user()->business->id)
+            ->orderBy('name', 'ASC')
+            ->paginate(10);
+
+
+        return view('livewire.scheduling.catalog.products', [
+            'products' => $products,
+            'categories' => $categories,
+        ]);
     }
 }
